b725396c5d60658014621ba78aaa4c40116dc2a4
Nemoto99 <gaeul9717@gmail.com>
2024-04-30 10:57:52 +0900

    test3

--- src/components/ChakraCard.js
+++ src/components/ChakraCard.js
@@ -63,7 +63,7 @@ const ChakraCard = () => {
                   fontSize='3xl'
                   mb={3}
                 >
-                  Shunichi Nemoto
+                  Shunichi Nemoto20
                 </Heading>
                 <Text
                   fontWeight='bold'
